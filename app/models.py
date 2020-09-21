33cb9a653c84f10efb29f3933bac214e7ebb1e4e
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-21 17:28:08 +0300

    Create upvote module to define each instance of an new upvote

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from . import db,login_manager
 from datetime import datetime
 from flask_login import UserMixin
+from flask_login import UserMixin,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
+
 class User(db.Model,UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
@@ -12,7 +14,9 @@ class User(db.Model,UserMixin):
     profile_pic_path = db.Column(db.String())
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
-    
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+
+
     @property
     def set_password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -34,29 +38,26 @@ class User(db.Model,UserMixin):
 class Pitch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(255))
-    post = db.Column(db.Text())
     title = db.Column(db.String(255),nullable = False)
     post = db.Column(db.Text(), nullable = False)
     comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     time = db.Column(db.DateTime, default = datetime.utcnow)
-    category = db.Column(db.String(255), index = True)
     category = db.Column(db.String(255), index = True,nullable = False)
-
+    
     def save_p(self):
         db.session.add(self)
-@@ -58,9 +58,9 @@ def __repr__(self):
+        db.session.commit()
+        
+    def __repr__(self):
+        return f'Pitch {self.post}'
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
-    comment = db.Column(db.Text())
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     comment = db.Column(db.Text(),nullable = False)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'),nullable = False)
-
     def save_c(self):
         db.session.add(self)
         db.session.commit()
@@ -67,7 +68,42 @@ class Comment(db.Model):
     
     def __repr__(self):
         return f'comment:{self.comment}'
-    
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    upvote = db.Column(db.Integer,default=1)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def add_upvotes(cls,id):
+        upvote_pitch = Upvote(user = current_user, pitch_id=id)
+        upvote_pitch.save()
+
+    @classmethod
+    def get_upvotes(cls,id):
+        upvote = Upvote.query.filter_by(pitch_id=id).all()
+        return upvote
+
+    @classmethod
+    def all_upvotes(cls,pitch_id):
+        upvotes = Upvote.query.order_by('id').all()
+        return upvotes
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+
+
+
+
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
