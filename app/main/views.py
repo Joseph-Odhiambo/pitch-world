2bf211e6179c8d684a1623802381a68948127892
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-21 19:01:47 +0300

    pitch world

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,9 @@
 from flask import render_template, redirect, url_for,abort,request
 from . import main
 from flask_login import login_required,current_user
-from ..models import User,Pitch,Comment,Upvote
-from .form import UpdateProfile,UpvoteForm,PitchForm,CommentForm
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .form import UpdateProfile,PitchForm,CommentForm
 from .. import db,photos
-
 
 @main.route('/')
 def index():
@@ -14,7 +13,6 @@ def index():
     advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
     return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
 
-@main.route('/new_pitch', methods = ['POST','GET'])
 @main.route('/create_new', methods = ['POST','GET'])
 @login_required
 def new_pitch():
@@ -24,11 +22,10 @@ def new_pitch():
         post = form.post.data
         category = form.category.data
         user_id = current_user
-        new_pitch = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
-        new_pitch.save_p()
         new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
         new_pitch_object.save_p()
         return redirect(url_for('main.index'))
+        
     return render_template('create_pitch.html', form = form)
 
 @main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
@@ -45,12 +42,18 @@ def comment(pitch_id):
         new_comment.save_c()
         return redirect(url_for('.comment', pitch_id = pitch_id))
     return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+
+
 @main.route('/user/<name>')
 def profile(name):
     user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    posts = Pitch.query.filter_by(user_id = user_id).all()
     if user is None:
         abort(404)
-    return render_template("profile/profile.html", user = user)
+
+    return render_template("profile/profile.html", user = user,posts=posts)
+
 @main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
 @login_required
 def updateprofile(name):
@@ -63,6 +66,8 @@ def updateprofile(name):
         user.save_u()
         return redirect(url_for('.profile',name = name))
     return render_template('profile/update.html',form =form)
+
+
 @main.route('/user/<name>/update/pic',methods= ['POST'])
 @login_required
 def update_pic(name):
@@ -74,16 +79,34 @@ def update_pic(name):
         db.session.commit()
     return redirect(url_for('main.profile',name=name))
 
-
 @main.route('/like/<int:id>',methods = ['POST','GET'])
 @login_required
 def like(id):
-    user = Upvote.query.get(id)
-    users = User.filter_by(id = id).first()
-    if users is None:
-        return redirect(url_for('auth.login',user = user))
-    all_upvotes = Upvote.query.filter_by(id=id).all()
-    pitch = Pitch.query.get(id)
-    new_vote = Upvote(user = user, pitch = pitch,upvote = 1)
+    get_pitches = Upvote.get_upvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for pitch in get_pitches:
+        to_str = f'{pitch}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_vote = Upvote(user = current_user, pitch_id=id)
     new_vote.save()
-    return redirect(url_for('main.index',id = pitch.id))
+    return redirect(url_for('main.index',id=id))
+
+@main.route('/dislike/<int:id>',methods = ['POST','GET'])
+@login_required
+def dislike(id):
+    pitch = Downvote.get_downvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for p in pitch:
+        to_str = f'{p}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_downvote = Downvote(user = current_user, pitch_id=id)
+    new_downvote.save()
+    return redirect(url_for('main.index',id = id))
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME=josephkdo@gmail.com
+export MAIL_PASSWORD=Kenyajo1
+python3.8 manage.py server
