577a0d622d368c3f2453c982cc3410ce20ebb391
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-22 10:25:34 +0300

    added immage

--- README.md
+++ README.md
@@ -58,7 +58,7 @@ Open the application on your browser `127.0.0.1:5000`.
 
 
 ## Known Bugs
-* There are no known bugs currently but pull requests are allowed incase you spot a bug
+<!-- * There are no known bugs currently but pull requests are allowed incase you spot a bug -->
 
 ## Contact Information 
 
--- tests/user_test.py
+++ tests/user_test.py
@@ -4,7 +4,7 @@ from app.models import User
 
 class UserTest(unittest.TestCase):
     def setUp(self):
-        self.new_user = User(username='joseph',password='odhiambo')
+        self.new_user = User(username='moringa',password='Access')
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.password is not None)
@@ -14,4 +14,4 @@ class UserTest(unittest.TestCase):
             self.new_user.password
 
     def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('odhiambo'))
+        self.assertTrue(self.new_user.verify_password('Access'))
