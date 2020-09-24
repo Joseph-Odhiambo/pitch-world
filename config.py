a7952d11c195392dceb4f3395b39723dc1898179
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-24 20:47:19 +0300

    config.py

--- config.py
+++ config.py
@@ -1,9 +1,33 @@
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+
+
+
+# class Config:
+#     SQLALCHEMY_TRACK_MODIFICATIONS=True
+#     SECRET_KEY=os.environ.get('SECRET_KEY')
+#     UPLOADED_PHOTOS_DEST ='app/static/photos'
+#     MAIL_SERVER = 'smtp.gmail.com'
+#     MAIL_PORT = 465
+#     MAIL_USE_TLS = False
+#     MAIL_USE_SSL = True
+#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+
+# class ProdConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+# class DevConfig(Config):
+#     DEBUG = True
+
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig
+
+# }
 import os
-from dotenv import load_dotenv
-load_dotenv()
-
-
-
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY=os.environ.get('SECRET_KEY')
@@ -14,16 +38,12 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
-
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
     DEBUG = True
-
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
-
 }
