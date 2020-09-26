68570ba60e7326a55b8058ab918ad83328c6c99e
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-26 14:33:23 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-    export MAIL_USERNAME=josephkdo@gmail.com
-export MAIL_PASSWORD=Kenyajo1
+    export MAIL_USERNAME=MAIL_USERNAME
+export MAIL_PASSWORD=MAIL_PASSWORD
 # export SECRET_KEY=
 
 python3.8 manage.py run
